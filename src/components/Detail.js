8ca08b894b467209a5c2f5410d9c9c46c4e269fa
iansexton <mydesignguy@gmail.com>
2023-11-30 23:41:11 -0500

    ga-worked in Wikimedia image API to get better iamges

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,11 +1,38 @@
 import React, { useEffect, useContext, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { DataContext } from '../Contexts/DataContext';
+import { type } from '@testing-library/user-event/dist/type';
+
+/*/--------------  WIKIMEDIA  ----------------/*/
+
+async function fetchWMData(searchTerm) {
+  try {
+    let url =
+      'https://commons.wikimedia.org/w/api.php?action=query&format=json&generator=search&gsrsearch=' +
+      encodeURIComponent(searchTerm) +
+      '&gsrnamespace=6&gsrlimit=1&prop=imageinfo&iiprop=url&iiurlwidth=1500&origin=*';
+    let response = await fetch(url, {
+      headers: {
+        Authorization: process.env.REACT_APP_WM_TOKEN,
+        'Api-User-Agent': 'Venues',
+      },
+    });
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error('Error:', error);
+    throw error; // Re-throw the error to handle it outside this function if needed
+  }
+}
+
+/*/--------------  END  ----------------/*/
 
 function Detail() {
   const dataContext = useContext(DataContext);
   const [thisVenue, setThisVenue] = useState(null);
-  const [venueImg, setVenuImg] = useState('');
+  const [venueImg, setVenueImg] = useState('');
   const [imgDataset, setImgDataset] = useState('');
   const [imgResults, setImgResults] = useState('');
 
@@ -20,10 +47,21 @@ function Detail() {
   useEffect(() => {
     if (dataContext.results.length > 0 && id) {
       setThisVenue(dataContext.results[0]);
+      console.log('venue', dataContext.results[0]);
     }
-  }, [dataContext.results, id]);
+    async function getUsableImage() {
+      try {
+        const vimg = await fetchWMData(dataContext.results[0].name);
+        const whichPage = Object.keys(vimg.query.pages)[0];
+        const WMimg = vimg.query.pages[whichPage].imageinfo[0].thumburl;
+        setVenueImg(WMimg);
+        return WMimg;
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    }
+  }, [dataContext.results, id, venueImg]);
 
-  console.log('dataContext.results', dataContext.results);
   // setPageTitle(thisVenue.name);
   // if (!id) {
   //   return <h1>Venue detail not found</h1>;
@@ -34,7 +72,7 @@ function Detail() {
       {thisVenue ? (
         <main id='detail'>
           <section className='dao'>
-            <figure>{thisVenue.images && thisVenue.images.length > 0 ? <img src={thisVenue.images[0].url} alt={thisVenue.name} /> : 'noimg'}</figure>
+            <figure>{thisVenue.images && thisVenue.images.length > 0 ? <img src={venueImg} alt={thisVenue.name} /> : 'noimg'}</figure>
 
             <section className='info'>
               <p> {thisVenue.generalInfo.generalRule} </p>
